d46b42bc41c16b5f7e92daa01f8d9866e69025ce
Hamza <hajjamhamza33@gmail.com>
2021-11-13 21:50:03 -0500

    testfin

--- TP214ETests/Data/DALTests.cs
+++ TP214ETests/Data/DALTests.cs
@@ -9,82 +9,16 @@ namespace TP214E.Data.Tests
     [TestClass()]
     public class DALTests
     {
-        private const bool congele = true;
-        private const string nom_valide = "nom";
-        private const string nom_Invalide = "";
-        private const string description_valide = "description";
-        private const string description_Invalide = "";
-        private const int quantite_valide = 3;
-        private const double prix_valide = 3;
-        private DateTime dateExpiration_valide = new DateTime(DateTime.MaxValue.Ticks);
-
         [TestMethod()]
         public void Teste_Ajout_recette_Dal()
         {
-            var DALMock = new Mock<IDAL>();
 
-            //configuration du mock pour que la fausse voiture retourne True 
-            //si sa méthode peutPrendreLaRoute est appelée
-
-            DALMock.Setup(x => x.AjouterRecette(Recette_Test_objet())).Returns(true);
-
-            DALMock.Verify(x => x.AjouterRecette(Recette_Test_objet()));
         }
 
         [TestMethod()]
         public void Teste_Ajout_Aliment_Dal()
         {
             var DALMock = new Mock<IDAL>();
-            
-            //configuration du mock pour que la fausse voiture retourne True 
-            //si sa méthode peutPrendreLaRoute est appelée
-
-            DALMock.Setup(x => x.AjouterRecette(Recette_Test_objet())).Returns(true);
-
-            DALMock.Setup(x => x.AjouterAliment(Get_Aliment_Test())).Returns(true);
-        }
-
-
-        private void lol()
-        {
-            //var voitureMock = new Mock<IVoiture>();
-
-            ////configuration du mock pour que la fausse voiture retourne True 
-            ////si sa méthode peutPrendreLaRoute est appelée
-            //voitureMock.Setup(x => x.peutPrendreLaRoute()).Returns(true);
-
-            //var garage = new Garage();
-
-            ////act
-            ////on appel la fonction que l'on veut tester avec notre faux objet
-            //garage.TesterVoiture(voitureMock.Object);
-
-            ////assert
-            //voitureMock.Verify(x => x.Roule());
-        }
-
-        private Recette Recette_Test_objet()
-        {
-            List<Ingredient> lsIngredients = new List<Ingredient>();
-
-            for (int i = 0; i < 3; i++)
-            {
-                Aliment alimentTest = new Aliment(nom_valide + i, quantite_valide, congele, dateExpiration_valide);
-
-                Ingredient ingredient = new Ingredient(alimentTest, 1);
-
-                lsIngredients.Add(ingredient);
-            }
-
-            Recette recette = new Recette(nom_valide, lsIngredients);
-
-            return recette;
-        }
-
-        private Aliment Get_Aliment_Test()
-        {
-            Aliment alimentTest = new Aliment(nom_valide, quantite_valide, congele, dateExpiration_valide);
-            return alimentTest;
         }
     }
 }
--- TP214ETests/Data/InventaireTests.cs
+++ TP214ETests/Data/InventaireTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using TP214E.Data;
 
@@ -7,31 +9,111 @@ namespace TP214E.Data.Tests
     [TestClass()]
     public class InventaireTests
     {
+        private const bool congele = true;
+        private const string nom_valide = "nom";
+        private const string description_valide = "description";
+        private const int quantite_valide = 3;
+        private const double prix_valide = 3;
+        private DateTime dateExpiration_valide = new DateTime(DateTime.MaxValue.Ticks);
+
         [TestMethod()]
-        public void Test()
+        public void TestConstructeurVide()
         {
             Inventaire inventaire = new Inventaire();
 
-
+            Assert.AreEqual(0,inventaire.LstRecettes.Count);
+            Assert.AreEqual(0,inventaire.LstAliments.Count);
+            Assert.AreEqual(0,inventaire.LstCommandesTraite.Count);
+            Assert.AreEqual(0,inventaire.LstPlats.Count);
         }
 
-
-        private void lol()
+        [TestMethod()]
+        public void TestInventaireAjoutAliment()
         {
-            //var voitureMock = new Mock<IVoiture>();
+            Inventaire inventaire = new Inventaire();
 
-            ////configuration du mock pour que la fausse voiture retourne True 
-            ////si sa méthode peutPrendreLaRoute est appelée
-            //voitureMock.Setup(x => x.peutPrendreLaRoute()).Returns(true);
+            inventaire.AjouterAliment(Get_Aliment_Test());
 
-            //var garage = new Garage();
+            Assert.AreEqual(1, inventaire.LstAliments.Count);
+        }
 
-            ////act
-            ////on appel la fonction que l'on veut tester avec notre faux objet
-            //garage.TesterVoiture(voitureMock.Object);
+        [TestMethod()]
+        public void TestInventaireSupressionAliment()
+        {
+            Inventaire inventaire = new Inventaire();
 
-            ////assert
-            //voitureMock.Verify(x => x.Roule());
+            Aliment aliment = Get_Aliment_Test();
+
+            inventaire.AjouterAliment(aliment);
+
+            inventaire.SupprimerAliment(aliment);
+
+            Assert.AreEqual(0, inventaire.LstAliments.Count);
+        }
+
+        [TestMethod()]
+        public void TestInventaireAjoutCommande()
+        {
+            Inventaire inventaire = new Inventaire();
+
+            inventaire.AjouterCommande(Commande_Test_Object());
+
+            Assert.AreEqual(1, inventaire.LstCommandesTraite.Count);
+        }
+
+        [TestMethod()]
+        public void TestInventaireSupressionCommande()
+        {
+            Inventaire inventaire = new Inventaire();
+
+            Commande commande = Commande_Test_Object();
+
+            inventaire.AjouterCommande(commande);
+
+            inventaire.SupprimerCommande(commande);
+
+            Assert.AreEqual(0, inventaire.LstCommandesTraite.Count);
+        }
+
+        private Aliment Get_Aliment_Test()
+        {
+            Aliment alimentTest = new Aliment(nom_valide, quantite_valide, congele, dateExpiration_valide);
+            return alimentTest;
+        }
+
+        private Recette recette_Test_objet()
+        {
+            List<Ingredient> lsIngredients = new List<Ingredient>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Aliment alimentTest = new Aliment(nom_valide + i, quantite_valide, congele, dateExpiration_valide);
+
+                Ingredient ingredient = new Ingredient(alimentTest, 1);
+
+                lsIngredients.Add(ingredient);
+            }
+
+            Recette recette = new Recette(nom_valide , lsIngredients);
+
+            return recette;
+        }
+
+        private Commande Commande_Test_Object()
+        {
+            Plat platTest = new Plat(nom_valide, prix_valide, description_valide, recette_Test_objet(), TypeDePlat.Burger);
+
+            PlatCommande platCommandeTest = new PlatCommande(platTest, quantite_valide);
+
+            List<PlatCommande> lstPlatCommandestest = new List<PlatCommande>();
+
+            lstPlatCommandestest.Add(platCommandeTest);
+
+            Commande commande = new Commande();
+
+            commande.PlatsCommandes = lstPlatCommandestest;
+
+            return commande;
         }
     }
 }
